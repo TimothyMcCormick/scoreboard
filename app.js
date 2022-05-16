3287642c37fccabb10a426ea442624c36fea9b81
Timothy McCormick <breaking490@gmail.com>
2022-05-16 16:15:17 -0600

    0516-sb-addedDrawFunction

--- app.js
+++ app.js
@@ -3,37 +3,39 @@ let homeScore = 0
 let awayScore = 0
 
 function incHome(){
-  let homeTotal = document.getElementById('homeScore')
-  homeScore ++
-  homeTotal.innerText = homeScore.toString()
+  homeScore++
+  draw()
+}
+
+function incHome3(){
+  homeScore += 3
+  draw()
 }
 
 function incAway(){
-  let awayTotal = document.getElementById('awayScore')
-  awayScore ++
-  awayTotal.innerText = awayScore.toString()
-}
-  
-function incHome3(){
-  let homeTotal = document.getElementById('homeScore')
-  homeScore += 3
-  homeTotal.innerText = homeScore.toString()
+  awayScore++
+  draw()
 }
 
 function incAway3(){
-  let awayTotal = document.getElementById('awayScore')
   awayScore += 3
-  awayTotal.innerText = awayScore.toString()
+  draw()
 }
 
 function resetScore(){
-  let homeTotal = document.getElementById('homeScore')
-  let awayTotal = document.getElementById('awayScore')
   homeScore = 0
   awayScore = 0
+  draw()
+}
+
+function draw(){
+  let homeTotal = document.getElementById('homeScore')
+  let awayTotal = document.getElementById('awayScore')
+
   homeTotal.innerText = homeScore.toString()
   awayTotal.innerText = awayScore.toString()
 }
+
   
   
   
